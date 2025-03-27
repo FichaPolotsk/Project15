6cb9708457c1abc31434ad89840971576fea193d
user <user@DESKTOP-0950516>
2025-03-27 19:53:02 +0300

    Добавьте файлы проекта.

---
+++ Project15/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	int choose = 10;
+	choose = 100 % 9;
+	if (choose == 1 and choose == 2) {
+		std::cin >> choose;
+	}
+	else if (choose == 1) {
+		std::cout << "2";
+	}
+	else {
+		std::cout << "1";
+	}
+
+
+
+}
